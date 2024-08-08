fb6e9828663f00806ccea6ed69c4c99b71cf8ed6
Your Name <you@example.com>
2024-08-08 11:11:06 -0500

    .

--- sunday/js/script.js
+++ sunday/js/script.js
@@ -1,27 +1,37 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 1; // Inicializa o índice do slide atual como 1
+showSlides(slideIndex); // Exibe o slide inicial
 
-// Next/previous controls
+
+// Função para avançar ou retroceder os slides
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showSlides(slideIndex += n);// Atualiza o índice do slide e exibe o slide correspondente
 }
 
-// Thumbnail image controls
+// Função para mostrar um slide específico com base no índice fornecido
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showSlides(slideIndex = n); // Define o índice do slide atual e exibe o slide correspondente
 }
 
+// Função para exibir slides
 function showSlides(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides"); //acessa as imagens pelo nome da classe "myslides"
+    let slides = document.getElementsByClassName("mySlides"); // Obtém todos os elementos com a classe "mySlides"
+    
+    // Se o índice do slide for maior que o número total de slides, redefine para 1
     if (n > slides.length) {
         slideIndex = 1;
     }
+    
+    // Se o índice do slide for menor que 1, redefine para o último slide
     if (n < 1) {
         slideIndex = slides.length;
     }
+
+    // Oculta todos os slides
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+
+    // Exibe o slide atual (baseado no índice ajustado)
     slides[slideIndex - 1].style.display = "block";
 }
